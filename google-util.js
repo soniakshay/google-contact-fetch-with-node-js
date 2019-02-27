690046ed7042ceb0daa665d1d7422d02fe39960d
soniakshay <soniiakshayy8000@gmail.com>
2019-02-27 15:00:27 +0530

    token set in localstorage

--- google-util.js
+++ google-util.js
@@ -1,10 +1,9 @@
 const {google} = require('googleapis');
 var express=require('express');
 var url = require('url');
-var contacts = google.people('v1');
-var localStorage = require('localStorage');
 var ls = require('local-storage');
 var app=express();
+
 
 
 const googleConfig = {
@@ -12,11 +11,15 @@ const googleConfig = {
     clientSecret: '5hsm7F0gnvEWFZ5u8gq_moGB', // e.g. _ASDFA%DFASDFASDFASD#FAD-
     redirect: 'http://localhost:3000/red' // this must match your google api settings
   };
+
+
+
   const oauth2Client = new google.auth.OAuth2(
     '1086091090723-9lt5ogj7gqh6tqu85v6o7vnif2pvq1t7.apps.googleusercontent.com',
     '5hsm7F0gnvEWFZ5u8gq_moGB',
     'http://localhost:3000/red'
   );
+
   function createConnection() {
     return new google.auth.OAuth2(
       googleConfig.clientId,
@@ -24,6 +27,10 @@ const googleConfig = {
       googleConfig.redirect
     );
   }
+
+
+
+
   const defaultScope = [
     'https://www.googleapis.com/auth/plus.me',
     'https://www.googleapis.com/auth/userinfo.email',
@@ -47,34 +54,41 @@ function urlGoogle() {
 
 
   app.get('/',function(red,res){
-    res.redirect(urlGoogle());
-});
+     res.redirect(urlGoogle());
+  });
 
 app.get('/red',async function(red,response){
-  var q = url.parse(red.url, true);
-  const {tokens} = await oauth2Client.getToken(q.query.code);
-  oauth2Client.setCredentials(tokens);
-
   
-  const peopleService = google.people({
-     version: 'v1', 
-     auth: oauth2Client
-  });
-
-
-  peopleService.people.connections.list({
-        resourceName: 'people/me',
-        personFields: 'names,emailAddresses'
-        },(err, res) => {
-          if(err){
-            console.log(err);
-          }else{
-            response.send(JSON.stringify(res.data.connections));
-          }
-  });
+    var q = url.parse(red.url, true);
+    const {tokens} = await oauth2Client.getToken(q.query.code);
+    oauth2Client.setCredentials(tokens);
+    ls.set('rtoken',tokens);
+    response.redirect('/contact'); 
+  
       
 });
 
+
+app.get('/contact',function(red,response){
+  oauth2Client.setCredentials(ls.get('rtoken'));
+  const peopleService = google.people({
+    version: 'v1', 
+    auth: oauth2Client
+  });
+  peopleService.people.connections.list({
+       resourceName: 'people/me',
+      personFields: 'names,phoneNumbers'
+      },(err, res) => {
+         if(err)
+         {
+           console.log(err);
+         }
+         else
+         {
+           response.send(JSON.stringify(res.data.connections));
+        }
+  });
+});
 app.listen(3000);
 
 module.exports=app;
